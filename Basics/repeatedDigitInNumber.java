373d23872d7b88477bd31c3d5e40eae26db95bef
Aniket Goyal <aniketgoyal291@gmail.com>
2023-07-03 18:49:07 +0530

    Commiting to save

--- Basics/repeatedDigitInNumber.java
+++ Basics/repeatedDigitInNumber.java
@@ -1,11 +1,23 @@
 package Basics;
 
+import java.util.Scanner;
+
 public class repeatedDigitInNumber {
 
     public static void main(String[] args) {
 
-        long a = 231352424212351l;
-        String count = Long.toString(a);
+        long a = 2313524333312351l;
+
+        int count = 0;
+        while (a > 0) {
+            int digit = (int) a % 10;
+            if (digit == 3) {
+                count += 1;
+            }
+            a = a / 10;
+        }
+
+        System.out.println(count);
 
     }
 }
