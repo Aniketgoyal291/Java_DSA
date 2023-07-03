8262a405154cd6242eb846bf11f72ec59b84715c
Aniket Goyal <aniketgoyal291@gmail.com>
2023-07-03 17:59:41 +0530

    Commiting to save

---
+++ TypeCasting.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Type conversion can only happend when the left hand side data type
+        // is greater than the right side data type.
+        // float num = 23.31f;
+
+        // type casting
+        // int intNum = (int) num;
+
+        // Automatic type promotion in expressions
+        // int a = 257;
+        // byte b = (byte) a; // 257%256 = 1;
+        // System.out.println(b);
+
+        // byte x = 40;
+        // byte y = 50;
+        // byte z = 100;
+        // int c = x * y / z; // The type is converted to int.
+        // Whenever we multiply 2 bytes, automatically converts into int.
+        // byte k = x*y;  ---> will give error.
+        // System.out.println(c); 
+
+        // int number = 'A'; // ASCI Value
+
+        byte b = 42;
+        char c = 'a';
+        short s = 1024;
+        int i = 50000;
+        float f = 5.67f;
+        double d = 0.1234;
+        double result = (f * b) + (i / c) - (d - s);
+        System.out.println((f * b) + " " + (i / c) + " " + +(d - s));
+        System.out.println(result);
+
+    }
+}
---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+
+        String str = "         Aniket goyal is my name";
+        System.out.println(str.trim());
+
+    }
+}
