5d70a344ed03f4040ab9fddc9ba0d6b06c8fbb82
Aniket Goyal <aniketgoyal291@gmail.com>
2023-07-03 11:56:25 +0530

    testing file commited

---
+++ testing.java
@@ -0,0 +1,10 @@
+public class testing {
+    public static void main(String[] args) {
+
+        int[] a = { 3, 2, 5, 3 };
+        int[] b = a;
+        a[0] = 10;
+        System.out.println(b[0]);
+
+    }
+}
