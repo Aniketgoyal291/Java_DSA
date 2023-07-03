e829f80f0355c926fde9cbd3ed6248dd0a02c466
Aniket Goyal <aniketgoyal291@gmail.com>
2023-07-03 18:41:27 +0530

    Commiting to save

--- Basics/repeatedDigitInNumber.java
+++ Basics/repeatedDigitInNumber.java
@@ -1,7 +1,11 @@
+package Basics;
+
 public class repeatedDigitInNumber {
+
     public static void main(String[] args) {
 
         long a = 231352424212351l;
+        String count = Long.toString(a);
 
     }
 }
--- Basics/test.java
+++ Basics/test.java
@@ -1,3 +1,5 @@
+package Basics;
+
 public class test {
     public static void main(String[] args) {
 
