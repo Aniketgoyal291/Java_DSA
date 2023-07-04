6a5d79fd332deae8eb0abd3e56e01c5ae675311f
Aniket Goyal <aniketgoyal291@gmail.com>
2023-07-04 20:13:30 +0530

    commited changes

---
+++ Methods/src/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+
+    public static void main(String[] args) {
+
+        String greet = greeting();
+        System.out.println(greet);
+
+    }
+
+    static String greeting(){
+        return "Hi, How are you";
+    }
+
+}
--- Methods/src/functions.java
+++
@@ -1,9 +0,0 @@
-public class functions {
-
-    public static void main(String[] args) {
-
-        System.out.println("My name is Aniket Goyal");
-
-    }
-
-}
